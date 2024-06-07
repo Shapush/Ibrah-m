9015405b1367c024e89d52f0121a35195802306d
Shapush <aynurenerimanova39@gmail.com>
2024-06-07 23:50:08 +0400

    Update Product.h

--- Product.h
+++ Product.h
@@ -1,1 +1,16 @@
 # Ibrah-m
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+class Product {
+public:
+    Product(const std::string& name, double price);
+    std::string getName() const;
+    double getPrice() const;
+
+private:
+    std::string name;
+    double price;
+};
+
+#endif
